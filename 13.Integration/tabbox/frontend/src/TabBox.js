8241115a95f10254049576bdcd1092de5337f025
dana-developer <hdy78901@naver.com>
2025-02-10 17:16:58 +0900

    integration tabbox tabview component add #1

--- 13.Integration/tabbox/frontend/src/TabBox.js
+++ 13.Integration/tabbox/frontend/src/TabBox.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Tabs from "./Tabs";
+import TabView from "./TabView";
 
 function TabBox({ tabs }) {
 	return (
 		<div className="tab-box">
 			<Tabs tabs={tabs}></Tabs>
-			<div>탭뷰입니다.</div>
+			<TabView></TabView>
 		</div>
 	);
 }
---
+++ 13.Integration/tabbox/frontend/src/TabView.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TabView(props) {
+	return <div>탭뷰입니다.</div>;
+}
+
+export default TabView;
