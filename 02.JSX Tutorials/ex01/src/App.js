baceec24905ffaced712bd6ea3d73e1b75a26d8e
dana-developer <hdy78901@naver.com>
2025-02-10 11:42:30 +0900

    basics finish #1

--- 02.JSX Tutorials/ex01/src/App.js
+++ 02.JSX Tutorials/ex01/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-    return (
-        <div>
-            <h1>Ex01</h1>
-            <p>특징 I: HTML과 비교</p>
-        </div>
-    );
+	return (
+		<div>
+			<h1>Ex01</h1>
+			<p>특징 I: HTML과 비교</p>
+		</div>
+	);
 }
 
-export {App};
+export { App };
--- 02.JSX Tutorials/ex01/src/index.js
+++ 02.JSX Tutorials/ex01/src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {App} from './App.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+if (process.env.NODE_ENV === "production") {
+	// 로그를 작성할 때에는 사용자가 보지 못하도록 함수를 재정의한다.
+	console.log = () => {};
+	console.info = () => {};
+	console.error = () => {};
+	console.warn = () => {};
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
