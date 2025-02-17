5fd0c1a2965211e63ff6bf9a7bcf4af82e1c2f8b
dana-developer <hdy78901@naver.com>
2025-02-17 10:19:24 +0900

    immutability finish

--- 08.Component - Immutability of State/ex02/src/App.js
+++ 08.Component - Immutability of State/ex02/src/App.js
@@ -1,38 +1,168 @@
-import React from 'react';
-import update from 'react-addons-update';import data from './assets/json/data.js';
+import React, { useEffect, useState } from "react";
+import update from "react-addons-update";
+import data from "./assets/json/data.js";
 
 function App() {
-    return (
-        <div id='App'>
-            <button>
-                {"배송지 수정"}
-            </button>
-            <br/><br/>
+	const [order, setOrder] = useState(data);
+	const [payment, setPayment] = useState(order.payment);
+	const [goods, setGoods] = useState(order.goods);
 
-            <button>
-                {"결제수단 변경"}
-            </button>
-            <br/><br/>
+	useEffect(() => {
+		console.log("Order Updated");
+	}, [order]);
 
-            <button>
-                {"상품 추가"}
-            </button>
-            <br/><br/>
+	useEffect(() => {
+		console.log("Payment Updated");
+	}, [payment]);
 
-            <button>
-                {"3rd 상품이름 변경"}
-            </button>
-            <br/><br/>
+	useEffect(() => {
+		console.log("Goods Updated");
+	}, [goods]);
 
-            <hr/>
+	return (
+		// React Addon에 복잡하고 중첩된 객체의 변경을 도와주는 immutablity helper 함수를 사용
+		<div id="App">
+			<button
+				onClick={() => {
+					// violation - 에러는 아니지만 위배되는 코드
+					// order.receive = '서울시 서초구 논현동...';
+					// setOrder(order);
 
-            <p>{`배송지:`}</p>
-            <p>{`결제수단:`}</p>
-            <p>{'상품'}</p>
-            <ul>
-            </ul>
-        </div>
-    );
+					// solution
+					// const orderUpdated = Object.assign({}, order, {receive: '서울시 서초구 논현동...'}); // (빈 객체, 복사할 source 객체, 바꿀 내용)
+					// setOrder(orderUpdated);
+
+					// solution.recommended: 프로퍼티
+					const orderUpdated = update(order, {
+						receive: {
+							$set: "서울시 서초구 논현동...",
+						},
+					}); //  const newObject = update(objectInState, { [WHERE] : { [WHAT]: updateValue } });
+					setOrder(orderUpdated);
+				}}
+			>
+				{"배송지 수정"}
+			</button>
+			<br />
+			<br />
+
+			<button
+				onClick={() => {
+					// 불변성을 지켜야 한다는 것에 대한 violation(직접 변경 불가)
+					// const orderUpdated = Object.assign({}, order);
+					// orderUpdated.payment.method = 'Mobile';
+					// setOrder(orderUpdated.payment);
+
+					// solution
+					// const orderUpdated = Object.assign({}, order);
+					// orderUpdated.payment = Object.assign({}, order.payment, {method: 'Mobile'});
+					// setPayment(orderUpdated.payment);
+
+					// sol.recommended: nest 객체 프로퍼티 수정
+					const orderUpdated = update(order, {
+						payment: {
+							method: {
+								$set: "Mobile",
+							},
+						},
+					});
+					setPayment(orderUpdated.payment);
+				}}
+			>
+				{"결제수단 변경"}
+			</button>
+			<br />
+			<br />
+
+			<button
+				onClick={() => {
+					// violation - 동일한 내용이면 추가가 안됨
+					// order.goods.push({
+					//     "no": "p002-001",
+					//     "name": "팬츠스트라이프",
+					//     "price": 2000,
+					//     "amount": 1});
+					// setGoods(goods);
+
+					// solution 01
+					// const goodsUpdate = goods.concat({
+					//     "no": "c002-003",
+					//     "name": "블루양말",
+					//     "price": 2000,
+					//     "amount": 1
+					// });
+					// setGoods(goodsUpdate);
+
+					// solution 02
+					// const goodsUpdate = [{
+					//     "no": "c002-003",
+					//     "name": "블루양말",
+					//     "price": 2000,
+					//     "amount": 1
+					// }, ...goods] // 배열 복사
+					// setGoods(goodsUpdate);
+
+					// solution.recommended: 배열 요소 추가
+					const goodsUpdate = update(goods, {
+						$unshift: [
+							{
+								no: "c002-003",
+								name: "블루양말",
+								price: 2000,
+								amount: 1,
+							},
+						], // unshift는 앞에 추가, push는 뒤에 추가
+					});
+					setGoods(goodsUpdate);
+				}}
+			>
+				{"상품 추가"}
+			</button>
+			<br />
+			<br />
+
+			<button
+				onClick={() => {
+					// violation
+					// goods[2].name = '블루면티'
+					// setGoods(goods);
+
+					// solution
+					// const goodsUpdate = [...goods.slice(0, 2),
+					//     Object.assign({}, goods[2], {name: '블루면티'}),
+					//     ...goods.slice(3)];
+					// setGoods(goodsUpdate);
+
+					// solution.recommended: 배열요소 수정
+					const goodsUpdate = update(goods, {
+						2: {
+							name: {
+								$set: "블루면티",
+							},
+						},
+					});
+					setGoods(goodsUpdate);
+				}}
+			>
+				{"3rd 상품이름 변경"}
+			</button>
+			<br />
+			<br />
+
+			<hr />
+
+			<p>{`배송지: ${order.receive}`}</p>
+			<p>{`결제수단: ${payment.method}`}</p>
+			<p>{"상품"}</p>
+			<ul>
+				{goods.map((good, index) => (
+					<li key={index}>
+						{`${good.name}:${good.price}:${good.amount}`}
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
-export {App};
+export { App };
--- 08.Component - Immutability of State/ex03/src/App.js
+++ 08.Component - Immutability of State/ex03/src/App.js
@@ -1,18 +1,51 @@
-import React from 'react';
-import data from './assets/json/data.js';
+import React, { useState } from "react";
+import data from "./assets/json/data.js";
+import update from "react-addons-update";
 
-function App() {
+function GoodList({ goods }) {
+	// goods.push({"no": "c002-003", "name": "블루양말", "price": 2000, "amount": 1});
+	const goodsUpdated = goods.concat({
+		no: "c002-003",
+		name: "블루양말",
+		price: 2000,
+		amount: 1,
+	});
 
-    return (
-        <div id='App'>
-            <p>{`배송지:`}</p>
-            <p>{`결제수단:`}</p>
-            <p>{'상품'}</p>
-            <button>
-                {"배송지 수정"}
-            </button>
-        </div>
-    );
+	return (
+		<ul>
+			{goodsUpdated.map((g, i) => (
+				<li
+					key={i}
+				>{`${g.name}:${g.amount}:${g.price}`}</li>
+			))}
+		</ul>
+	);
 }
 
-export {App};
+function App() {
+	const [order, setOrder] = useState(data);
+
+	return (
+		<div id="App">
+			<p>{`배송지: ${order.receive}`}</p>
+			<p>{`결제수단: ${order.payment.method}`}</p>
+			<p>{"상품"}</p>
+			<GoodList goods={order.goods} />
+			<button
+				onClick={() => {
+					setOrder(
+						update(order, {
+							receive: {
+								$set: "서울시 강남구 논현동....",
+							},
+						})
+					);
+				}}
+			>
+				{"배송지 수정"}
+			</button>
+		</div>
+	);
+}
+
+export { App };
